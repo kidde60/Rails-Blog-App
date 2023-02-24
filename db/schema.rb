705a60e73f1b2bd2b74f287e1f868b2cfb8f0252
kidde60 <kgeorgewilliam60@gmail.com>
2023-02-24 13:54:35 +0300

    Add migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_22_132857) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_23_152931) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,11 +18,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_22_132857) do
     t.string "Text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.bigint "posts_id", null: false
+    t.index ["posts_id"], name: "index_comments_on_posts_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.bigint "posts_id", null: false
+    t.index ["posts_id"], name: "index_likes_on_posts_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -32,6 +40,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_22_132857) do
     t.integer "LikesCounter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,4 +53,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_22_132857) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts", column: "posts_id"
+  add_foreign_key "comments", "users"
+  add_foreign_key "likes", "posts", column: "posts_id"
+  add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users"
 end
