9e36f159526961c5f9dea171d38d575f78e58e06
kidde60 <kgeorgewilliam60@gmail.com>
2023-02-25 22:53:06 +0300

    Add posts validation

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -1,34 +1,41 @@
 require 'rails_helper'
 
-describe Post, type: :model do
-  context 'test posts' do
-    before :each do
-      @user = User.create(name: 'william', photo: 'https://github.com/kidde60', bio: 'web developer')
-      @post = Post.create(author: @user, title: 'Title', text: 'My sample text')
+RSpec.describe Post, type: :model do
+  let(:user) { User.create(name: 'Test User', photo: 'test@example.com', bio: 'password', posts_counter: 0) }
+  let(:post) { Post.create(title: 'Test Post', text: 'Test content', author: user) }
+
+  context '#post_counter' do
+    it 'increments the author post counter' do
+      expect { post.posts_counter }.to change { user.reload.posts_counter }.by(1)
+    end
+  end
+
+  context '#five_most_recent_comments' do
+    it 'returns the 5 most recent comments' do
+      comments = post.five_most_recent_comments
+      expect(comments).to eq(post.comments.order(created_at: :desc).limit(5))
+    end
+  end
+
+  context 'validations' do
+    it 'requires a title' do
+      post.title = ''
+      expect(post).to_not be_valid
     end
 
-    it 'should not have an empty title' do
-      expect(@post.title).to eql 'Title'
+    it 'requires title to be no more than 250 characters' do
+      post.title = 'a' * 251
+      expect(post).to_not be_valid
     end
 
-    it 'should have 0 comments at default' do
-      expect(@post.comments_counter).to eq 0
+    it 'requires comments_counter to be an integer greater than or equal to 0' do
+      post.comments_counter = -1
+      expect(post).to_not be_valid
     end
 
-    it 'should not havae a title longer than 250 char' do
-      @post.title = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Pharetra sit amet aliquam id diam maecenas ultricies mi eget.
-        Commodo viverra maecenas accumsan lacus vel.
-        Platea dictumst vestibulum rhoncus est. Quisque egestas diam in arcu.'
-      expect(@post).to_not be_valid
-    end
-
-    it 'should return number of recent comments (5 max)' do
-      @comment = Comment.create(post: @post, author: @user, text: 'Awesome!')
-      @comment = Comment.create(post: @post, author: @user, text: 'Awesome!')
-      @comment = Comment.create(post: @post, author: @user, text: 'Awesome!')
-      expect(@post.recent_comments.length).to eq 3
+    it 'requires likes_counter to be an integer greater than or equal to 0' do
+      post.likes_counter = -1
+      expect(post).to_not be_valid
     end
   end
 end
