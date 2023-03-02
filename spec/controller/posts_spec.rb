8fe4e4c35521c4b2d1a16b4e3ca76c509ba4f56c
kidde60 <kgeorgewilliam60@gmail.com>
2023-03-02 11:51:45 +0300

    Add posts test

---
+++ spec/controller/posts_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts', type: :request do
+  describe 'GET/index' do
+    it 'Check if response status is correct' do
+      get '/users/:id/posts'
+      expect(response).to have_http_status(:ok)
+    end
+    it 'passes with status 200' do
+      get '/users/120/posts'
+      expect(response.status).to eql(200)
+    end
+
+    it 'includes correct text' do
+      get '/users/120/posts'
+      expect(response.body).to include('List of all posts by a user ')
+    end
+  end
+
+  describe 'GET/show' do
+    it 'Check if response status is correct' do
+      get '/users/:id/posts/:post_id'
+      expect(response).to have_http_status(:ok)
+    end
+    it 'passes with status 200' do
+      get '/users/100/posts/20'
+      expect(response.status).to eql(200)
+    end
+
+    it 'include correct text' do
+      get '/users/100/posts/2'
+      expect(response.body).to include('List of post by given id')
+    end
+  end
+end
