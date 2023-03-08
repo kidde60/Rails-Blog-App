5c68ddd6545cc6ff710a3280e7d31b0f4be04d7b
kidde60 <kgeorgewilliam60@.com>
2023-03-08 13:13:36 +0300

    Add posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,25 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
-    @posts = @user.posts.includes(:comments)
+    @user = User.find(params[:author_id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(author: current_user, title: params[:title], text: params[:text])
+    @post.author = current_user
+
+    if @post.save
+      redirect_to user_posts_path(id: @post.author_id)
+    else
+      render :new, alert: 'Error: Post not saved.'
+    end
   end
 
   def show
-    @user = User.find(params[:user_id])
+    @user = User.find(params[:author_id])
     @post = Post.find(params[:id])
-    @comments = @post.comments
   end
 end
