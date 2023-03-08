97bf53a02f7bfdac20ab75c1efcc250531624294
kidde60 <kgeorgewilliam60@.com>
2023-03-08 13:12:38 +0300

    Add comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,14 @@
+class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(post_id: params[:id], author: current_user, text: params[:text])
+    if @comment.save
+      redirect_to user_post_path(current_user.id, params[:id])
+    else
+      render :new, alert: 'Error: Comment not saved.'
+    end
+  end
+end
